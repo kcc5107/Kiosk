13584418228d750b0471bcb1940de1e174237dcb
Jang Gyeonghyuk <kcc5107@naver.com>
2025-04-24 14:40:26 +0900

    키오스크 과제Lv2 구현완료

--- src/com/myapp/kiosk/Main.java
+++ src/com/myapp/kiosk/Main.java
@@ -1,40 +1,46 @@
 package com.myapp.kiosk;
 
+import com.myapp.kiosk.menu.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        while(true) {
-            Scanner sc = new Scanner(System.in);
+        List<MenuItem> menuItems = new ArrayList<>(List.of(
+                new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"),
+                new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"),
+                new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"),
+                new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거")
+        ));
+        Scanner sc = new Scanner(System.in);
 
-            System.out.println("""
-            [ SHAKESHACK MENU ]\s
-            1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거\s
-            2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거\s
-            3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거\s
-            4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거\s
-            0. 종료""");
-            int menuSelect = sc.nextInt();
-            switch (menuSelect) {
-                case 1:
-                    System.out.println("ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거\n");
-                    break;
-                case 2:
-                    System.out.println("SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거\n");
-                    break;
-                case 3:
-                    System.out.println("Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거\n");
-                    break;
-                case 4:
-                    System.out.println("Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거\n");
-                    break;
-                case 0:
-                    System.out.println("프로그램을 종료합니다.");
-                    return;
-                default:
-                    break;
+        while (true) {
+//            System.out.println("""
+//                    [ SHAKESHACK MENU ]\s
+//                    1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거\s
+//                    2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거\s
+//                    3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거\s
+//                    4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거\s
+//                    0. 종료""");
+            System.out.println("[ SHAKESHACK MENU ]");
+            for (int i=0; i<menuItems.size(); i++) {
+                MenuItem menuItem = menuItems.get(i);
+                System.out.printf("%d %-13s | W %3.1f | %s\n", i+1, menuItem.getName(), menuItem.getPrice(), menuItem.getDescription());
             }
+            System.out.println("0. 종료");
 
+            int menuSelect = sc.nextInt();
+            if (menuSelect == 0 ) {
+                System.out.println("프로그램을 종료합니다.");
+                return;
+            } else if (menuSelect > 4) {
+                System.out.println("잘못된 입력입니다");
+                continue;
+            }
+            MenuItem menuItem = menuItems.get(menuSelect-1);
+            System.out.printf("선택한 메뉴 : %s | W %3.1f | %s%n ", menuItem.getName(), menuItem.getPrice(), menuItem.getDescription());
         }
     }
 }
---
+++ src/com/myapp/kiosk/menu/MenuItem.java
@@ -0,0 +1,27 @@
+package com.myapp.kiosk.menu;
+
+import jdk.jfr.Description;
+
+public class MenuItem {
+    private String name;
+    private double price;
+    private String description;
+
+    public MenuItem(String name, double price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
