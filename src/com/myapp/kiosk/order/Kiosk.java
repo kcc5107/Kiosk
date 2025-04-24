15f1dbbf15cea2f9356fd5ae5232999583685b02
Jang Gyeonghyuk <kcc5107@naver.com>
2025-04-24 16:15:35 +0900

    키오스크 과제Lv3 수정(예외처리)

--- src/com/myapp/kiosk/order/Kiosk.java
+++ src/com/myapp/kiosk/order/Kiosk.java
@@ -3,6 +3,7 @@ package com.myapp.kiosk.order;
 import com.myapp.kiosk.menu.MenuItem;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -14,24 +15,35 @@ public class Kiosk {
         this.menuItems = menuItems;
     }
 
-    public void start() {
+    public void start() throws InputMismatchException, IndexOutOfBoundsException {
         while (true) {
             System.out.println("[ SHAKESHACK MENU ]");
-            for (int i=0; i<menuItems.size(); i++) {
+            for (int i = 0; i < menuItems.size(); i++) {
                 MenuItem menuItem = menuItems.get(i);
-                System.out.printf("%d %-13s | W %3.1f | %s\n", i+1, menuItem.getName(), menuItem.getPrice(), menuItem.getDescription());
+                System.out.printf("%d %-13s | W %3.1f | %s\n", i + 1, menuItem.getName(), menuItem.getPrice(), menuItem.getDescription());
             }
             System.out.println("0. 종료");
 
-            int menuSelect = sc.nextInt();
-            if (menuSelect == 0 ) {
-                System.out.println("프로그램을 종료합니다.");
-                return;
-            } else if (menuSelect > 4) {
-                System.out.println("잘못된 입력입니다");
+            int menuSelect = 0;
+            try {
+                menuSelect = sc.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("숫자를 입력해주세요.");
+                sc.nextLine();
                 continue;
             }
-            MenuItem menuItem = menuItems.get(menuSelect-1);
+            if (menuSelect == 0) {
+                System.out.println("프로그램을 종료합니다.");
+                return;
+            }
+
+            MenuItem menuItem = null;
+            try {
+                menuItem = menuItems.get(menuSelect - 1);
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("지정된 범위의 숫자를 입력해주세요");
+                continue;
+            }
             System.out.printf("선택한 메뉴 : %s | W %3.1f | %s%n ", menuItem.getName(), menuItem.getPrice(), menuItem.getDescription());
         }
     }
